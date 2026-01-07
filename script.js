2015f0f0427ea6ebee0f23571ea515035ea3c393
Crx08 <cesare.sonaglia@gmail.com>
2026-01-07 17:30:47 +0100

    Caricamento avvenuto

--- script.js
+++ script.js
@@ -4,41 +4,35 @@ console.log(d);
 document.getElementById("saluto").innerHTML = d.getDate();
 
 let colori = [];
+
 function elabora(){
-    let numero = document.getElementById("inNumber").value;
+    let numero = Number(document.getElementById("inNumber").value);
     alert(numero);
 
+    let testo = document.getElementById("inText").value;
+    document.getElementById("outText2").innerHTML = testo;
+
     if(numero >= 0 && numero <= 255){
-        // iserirre il numero dentro l'array
+        // inserire il numero dentro l'array
         colori.push(numero);
-        // visuallizzare il numero dentro il div output
+
+        // visualizzare il numero dentro il div output
         document.getElementById("outText").innerHTML =
             "Hai inserito il numero " + numero;
 
-        // visuallizzare i numeri inseriti
+        // visualizzare i numeri inseriti
         document.getElementById("outText").innerHTML +=
-            "<br> I colori inseriti sono:" + colori;
-            console.log(colori);
+            "<br>I colori inseriti sono: " + colori.join(", ");
+        console.log(colori);
 
-            if(colori.length == 3){
-                newColor = `rgb(${colori[0]}, ${colori[1]}, ${colori[2]})`;
-                document.getElementById("outText").style.color += newColor;
-            }
+        if(colori.length == 3){
+            let newColor = `rgb(${colori[0]}, ${colori[1]}, ${colori[2]})`;
+            document.getElementById("outText2").style.color = newColor;
+        }
 
-    }else {
+    } else {
         // messaggio dentro il div error
-        document.getElementById("error").innerHTML = "Il numero inserito non va bene"
+        document.getElementById("error").innerHTML =
+            "Il numero inserito non va bene";
     }
-
-}
-
-
-
-
-
-
-
-function reset(){
-    document.getElementById("error").innerHTML = "";
-
 }
